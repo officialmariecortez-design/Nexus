79410a84ccfc214eb8e7b03783665956c5b5e52b
officialmariecortez-design <officialmariecortez@gmail.com>
2026-09-14 05:05:14 -0300

    Update pool.js

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -2,7 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: process.env.PGSSL_REJECT_UNAUTHORIZED !== "false" } : false
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 module.exports = pool;
